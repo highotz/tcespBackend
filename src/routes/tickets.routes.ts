26a9196f0a97be7ef9608b2089210d739241a6b7
Gabriel Lima <gabriel.lima@MacBook-de-Gabriel.local>
2021-10-09 17:44:22 -0300

    added update route

--- src/routes/tickets.routes.ts
+++ src/routes/tickets.routes.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import { ensureAuth } from "../middlewares/ensureAuth";
 import { CreateTicketController } from "../modules/tickets/useCases/createTicket/CreateTicketController";
 import { ListAllTicketsController } from "../modules/tickets/useCases/listAllTickets/ListAllTicketsController";
+import { UpdateTicketController } from "../modules/tickets/useCases/updateTicket/UpdateTicketController";
 
 const ticketsRoutes = Router();
 
 const createTicketController = new CreateTicketController();
 const listAllTicketsController = new ListAllTicketsController();
+const updateTicketController = new UpdateTicketController();
 
 ticketsRoutes.post(
   "/new-ticket/:user_id",
@@ -15,5 +17,6 @@ ticketsRoutes.post(
   createTicketController.handle
 );
 ticketsRoutes.get("/", ensureAuth, listAllTicketsController.handle);
+ticketsRoutes.put("/update-ticket", ensureAuth, updateTicketController.handle);
 
 export { ticketsRoutes };
